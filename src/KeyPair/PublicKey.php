8d3cc3b8276ea6c1366ab4a9679b5b538612172e
Furqan Siddiqui <hello@furqansiddiqui.com>
2020-09-01 12:54:39 +0500

    base public key

---
+++ src/KeyPair/PublicKey.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is a part of "furqansiddiqui/ethereum-php" package.
+ * https://github.com/furqansiddiqui/ethereum-php
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/ethereum-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Ethereum\KeyPair;
+
+use Comely\DataTypes\Buffer\Base16;
+use FurqanSiddiqui\BIP32\Extend\PrivateKeyInterface;
+use FurqanSiddiqui\ECDSA\ECC\EllipticCurveInterface;
+use FurqanSiddiqui\Ethereum\Ethereum;
+
+/**
+ * Class PublicKey
+ * @package FurqanSiddiqui\Ethereum\KeyPair
+ */
+class PublicKey extends \FurqanSiddiqui\BIP32\KeyPair\PublicKey
+{
+    /** @var Ethereum */
+    private Ethereum $eth;
+
+    /**
+     * PublicKey constructor.
+     * @param Ethereum $eth
+     * @param PrivateKeyInterface|null $privateKey
+     * @param EllipticCurveInterface|null $curve
+     * @param Base16|null $publicKey
+     * @param bool|null $pubKeyArgIsCompressed
+     * @throws \FurqanSiddiqui\BIP32\Exception\PublicKeyException
+     */
+    public function __construct(Ethereum $eth, ?PrivateKeyInterface $privateKey, ?EllipticCurveInterface $curve = null, ?Base16 $publicKey = null, ?bool $pubKeyArgIsCompressed = null)
+    {
+        $this->eth = $eth;
+        parent::__construct($privateKey, $curve, $publicKey, $pubKeyArgIsCompressed);
+    }
+
+    /**
+     * @return Ethereum
+     */
+    public function eth(): Ethereum
+    {
+        return $this->eth;
+    }
+}
