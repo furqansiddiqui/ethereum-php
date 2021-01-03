c2c97d49582b81f5c8c610f3febbfc445454cb81
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-01-03 18:08:07 +0500

    change V calculation

--- src/KeyPair/PrivateKey.php
+++ src/KeyPair/PrivateKey.php
@@ -61,6 +61,7 @@ class PrivateKey extends \FurqanSiddiqui\BIP32\KeyPair\PrivateKey
     /**
      * @param RLPEncodedTx $serializedTx
      * @return RLPEncodedTx
+     * @throws \FurqanSiddiqui\BIP32\Exception\PublicKeyException
      * @throws \FurqanSiddiqui\Ethereum\Exception\AccountsException
      * @throws \FurqanSiddiqui\Ethereum\Exception\IncompleteTxException
      */
@@ -68,11 +69,21 @@ class PrivateKey extends \FurqanSiddiqui\BIP32\KeyPair\PrivateKey
     {
         $curve = Curves::getInstanceOf($this->getEllipticCurveId());
         $signature = $curve->sign($this->base16(), $serializedTx->hash());
-        $pointR = $signature->curvePointR();
 
         // Check parity of Y coord of R
-        $parity = strlen(str_replace("0", "", gmp_strval($pointR->y(), 2))) % 2 === 0 ? 0 : 1;
-        $sigV = $this->eth->networkConfig()->chainId * 2 + (35 + $parity);
+        // $pointR = $signature->curvePointR();
+        // $parity = strlen(str_replace("0", "", gmp_strval($pointR->y(), 2))) % 2 === 0 ? 0 : 1;
+        // $sigV = $this->eth->networkConfig()->chainId * 2 + (35 + $parity);
+
+        $recId = $curve->findRecoveryId(
+            $this->publicKey()->getEllipticCurvePubKeyObj(),
+            $signature,
+            $serializedTx->hash(),
+            true
+        );
+
+        $sigV = $this->eth->networkConfig()->chainId * 2 + (4 + $recId);
+
         $txn = TxBuilder::Decode($this->eth, $serializedTx);
         $txn->signature($sigV, $signature->r(), $signature->s());
 
