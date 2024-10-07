4e2cffccf579a6afb517706ca81d5c3ee06b7768
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2024-10-08 03:17:18 +0500

    transaction fixes

--- src/RPC/Result/Transaction.php
+++ src/RPC/Result/Transaction.php
@@ -14,11 +14,11 @@ use FurqanSiddiqui\Ethereum\Exception\RPC_ResponseException;
 readonly class Transaction
 {
     public string $from;
-    public string $to;
+    public ?string $to;
     public string $hash;
     public WEIAmount $value;
     public int $gas;
-    public WEIAmount $gasPrice;
+    public ?WEIAmount $gasPrice;
     public int $nonce;
     public Buffer $input;
     public ?string $blockHash;
@@ -32,21 +32,21 @@ readonly class Transaction
      */
     public function __construct(array $result)
     {
-        if (!isset($result["from"], $result["to"], $result["hash"], $result["value"], $result["gas"],
-            $result["gasPrice"], $result["nonce"], $result["input"])) {
+        if (!isset($result["from"], $result["hash"], $result["value"], $result["gas"],
+            $result["nonce"], $result["input"])) {
             throw new RPC_ResponseException("Incomplete transaction data");
         }
 
         $this->from = $result["from"];
-        $this->to = $result["to"];
+        $this->to = $result["to"] ?? null;
         $this->hash = $result["hash"];
         $this->value = new WEIAmount($result["value"]);
         $this->gas = gmp_intval(gmp_init($result["gas"], 16));
-        $this->gasPrice = new WEIAmount($result["gasPrice"]);
+        $this->gasPrice = isset($result["gasPrice"]) ? new WEIAmount($result["gasPrice"]) : null;
         $this->nonce = gmp_intval(gmp_init($result["nonce"], 16));
         $this->input = (new Buffer($result["input"]))->readOnly();
         $this->blockHash = $result["blockHash"] ?? null;
-        $this->blockNumber = gmp_intval(gmp_init($result["blockNumber"], 16));
+        $this->blockNumber = isset($result["blockNumber"]) ? gmp_intval(gmp_init($result["blockNumber"], 16)) : null;
         $this->transactionIndex = isset($result["transactionIndex"]) ?
             gmp_intval(gmp_init($result["transactionIndex"], 16)) : null;
     }
