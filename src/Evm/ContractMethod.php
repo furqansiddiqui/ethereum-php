b18e6dbad1c2f965dc7e2d99d47b01e42d3273d1
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-10 23:49:14 +0400

    feat: add ContractMethod class to represent contract methods with type, name, inputs, outputs, and properties

---
+++ src/Evm/ContractMethod.php
@@ -0,0 +1,48 @@
+<?php
+/*
+ * Part of the "furqansiddiqui/ethereum-php" package.
+ * @link https://github.com/furqansiddiqui/ethereum-php
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Ethereum\Evm;
+
+/**
+ * Represents a method within a contract, including its type, name, inputs, outputs, and properties such as
+ * whether it is constant or payable.
+ */
+final class ContractMethod
+{
+    /** @var AbiParam[] */
+    private(set) array $inputs = [];
+    /** @var AbiParam[] */
+    private(set) array $outputs = [];
+
+    public function __construct(
+        public readonly string  $type,
+        public readonly ?string $name,
+        public readonly ?bool   $isConstant,
+        public readonly ?bool   $isPayable,
+    )
+    {
+    }
+
+    /**
+     * @param AbiParam $param
+     * @return void
+     */
+    public function appendInput(AbiParam $param): void
+    {
+        $this->inputs[] = $param;
+    }
+
+    /**
+     * @param AbiParam $param
+     * @return void
+     */
+    public function appendOutput(AbiParam $param): void
+    {
+        $this->outputs[] = $param;
+    }
+}
