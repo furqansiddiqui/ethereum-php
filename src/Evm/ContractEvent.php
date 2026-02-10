090083221244b9a22c61193dcb5b98386ca8f92c
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-10 23:46:40 +0400

    feat: add ContractEvent class to represent contract events with name, anonymity status, and input parameters

---
+++ src/Evm/ContractEvent.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * Part of the "furqansiddiqui/ethereum-php" package.
+ * @link https://github.com/furqansiddiqui/ethereum-php
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Ethereum\Evm;
+
+/**
+ * Represents a contract event, encapsulating its name, anonymity status,
+ * and associated input parameters.
+ */
+final class ContractEvent
+{
+    /** @var AbiParam[] */
+    private(set) array $inputs = [];
+
+    /**
+     * @param string $name
+     * @param bool $isAnonymous
+     */
+    public function __construct(
+        public readonly string $name,
+        public readonly bool   $isAnonymous,
+    )
+    {
+    }
+
+    /**
+     * @param AbiParam $param
+     * @return void
+     */
+    public function appendInput(AbiParam $param): void
+    {
+        $this->inputs[] = $param;
+    }
+}
