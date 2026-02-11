ec781f1fb0da4fdc1ac27a78f5d79b3023106b28
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-11 19:35:32 +0400

    refactor: streamline TxRlpSchema definitions, remove unused EIP-2718 mappers, and add schemas for EIP-2930 transactions

--- src/Transactions/TxRlpSchema.php
+++ src/Transactions/TxRlpSchema.php
@@ -11,20 +11,17 @@ namespace FurqanSiddiqui\Ethereum\Transactions;
 use FurqanSiddiqui\Ethereum\Codecs\RLP\RlpFieldType;
 use FurqanSiddiqui\Ethereum\Codecs\RLP\RlpSchema;
 
+/**
+ * Defines reusable RLP schemas for various Ethereum transaction types, including Legacy, EIP-1559, and EIP-2930
+ * (both signed and unsigned variants).
+ */
 final class TxRlpSchema
 {
     private static ?RlpSchema $legacyTx = null;
     private static ?RlpSchema $eip1559TxSigned = null;
     private static ?RlpSchema $eip1559TxUnsigned = null;
-
-    /** @var \FurqanSiddiqui\Ethereum\RLP\Mapper|null */
-    private static null|Mapper $eip1559Tx = null;
-    /** @var \FurqanSiddiqui\Ethereum\RLP\Mapper|null */
-    private static null|Mapper $eip1559Tx_Unsigned = null;
-    /** @var \FurqanSiddiqui\Ethereum\RLP\Mapper|null */
-    private static null|Mapper $eip2718Tx = null;
-    /** @var \FurqanSiddiqui\Ethereum\RLP\Mapper|null */
-    private static null|Mapper $eip2718Tx_Unsigned = null;
+    private static ?RlpSchema $eip2930TxSigned = null;
+    private static ?RlpSchema $eip2930TxUnsigned = null;
 
     /**
      * @return RlpSchema
@@ -47,6 +44,9 @@ final class TxRlpSchema
             ->add(RlpFieldType::Bytes32, "signatureS");
     }
 
+    /**
+     * @return RlpSchema
+     */
     public static function eip1559Tx(): RlpSchema
     {
         if (self::$eip1559TxSigned) {
@@ -90,47 +90,45 @@ final class TxRlpSchema
     }
 
     /**
-     * @return \FurqanSiddiqui\Ethereum\RLP\Mapper
+     * @return RlpSchema
      */
-    public static function EIP2718Tx(): Mapper
+    public static function eip2930Tx(): RlpSchema
     {
-        if (static::$eip2718Tx) {
-            return static::$eip2718Tx;
+        if (self::$eip2930TxSigned) {
+            return self::$eip2930TxSigned;
         }
 
-        static::$eip2718Tx = (new Mapper())
-            ->expectInteger("chainId")
-            ->expectInteger("nonce")
-            ->expectWEIAmount("gasPrice")
-            ->expectInteger("gasLimit")
-            ->expectAddress("to")
-            ->expectWEIAmount("value")
-            ->expectString("data")
-            ->mapAsIs("accessList")
-            ->expectBool("signatureParity")
-            ->expectString("signatureR")
-            ->expectString("signatureS");
-        return static::$eip2718Tx;
+        return self::$eip2930TxSigned = new RlpSchema()
+            ->add(RlpFieldType::Integer, "chainId")
+            ->add(RlpFieldType::Integer, "nonce")
+            ->add(RlpFieldType::Wei, "gasPrice")
+            ->add(RlpFieldType::Integer, "gasLimit")
+            ->add(RlpFieldType::AddressNullable, "to")
+            ->add(RlpFieldType::Wei, "value")
+            ->add(RlpFieldType::String, "data")
+            ->add(RlpFieldType::Include, "accessList")
+            ->add(RlpFieldType::Bool, "yParity")
+            ->add(RlpFieldType::Bytes32, "signatureR")
+            ->add(RlpFieldType::Bytes32, "signatureS");
     }
 
     /**
-     * @return \FurqanSiddiqui\Ethereum\RLP\Mapper
+     * @return RlpSchema
      */
-    public static function EIP2718Tx_Unsigned(): Mapper
+    public static function eip2930TxUnsigned(): RlpSchema
     {
-        if (static::$eip2718Tx_Unsigned) {
-            return static::$eip2718Tx_Unsigned;
+        if (self::$eip2930TxUnsigned) {
+            return self::$eip2930TxUnsigned;
         }
 
-        static::$eip2718Tx_Unsigned = (new Mapper())
-            ->expectInteger("chainId")
-            ->expectInteger("nonce")
-            ->expectWEIAmount("gasPrice")
-            ->expectInteger("gasLimit")
-            ->expectAddress("to")
-            ->expectWEIAmount("value")
-            ->expectString("data")
-            ->mapAsIs("accessList");
-        return static::$eip2718Tx_Unsigned;
+        return self::$eip2930TxUnsigned = new RlpSchema()
+            ->add(RlpFieldType::Integer, "chainId")
+            ->add(RlpFieldType::Integer, "nonce")
+            ->add(RlpFieldType::Wei, "gasPrice")
+            ->add(RlpFieldType::Integer, "gasLimit")
+            ->add(RlpFieldType::AddressNullable, "to")
+            ->add(RlpFieldType::Wei, "value")
+            ->add(RlpFieldType::String, "data")
+            ->add(RlpFieldType::Include, "accessList");
     }
 }
