076beffb2748e66dd0f090059d71a27a51be05b6
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-11 18:00:53 +0400

    refactor: rename TransactionInterface to EthereumTransactionInterface and update method signatures with adjusted types and documentation

--- src/Transactions/EthereumTransactionInterface.php
+++ src/Transactions/EthereumTransactionInterface.php
@@ -10,40 +10,42 @@ namespace FurqanSiddiqui\Ethereum\Transactions;
 
 use Charcoal\Buffers\Types\Bytes32;
 use Charcoal\Contracts\Buffers\ReadableBufferInterface;
-use FurqanSiddiqui\Ethereum\Ethereum;
 
 /**
  * Represents a standardized structure for handling transactions.
  * Provides methods to decode raw transactions, retrieve an unsigned version,
  * check the signature status, and generate transaction-related hashes.
  */
-interface TransactionInterface
+interface EthereumTransactionInterface
 {
     /**
-     * @param Ethereum $eth
-     * @param ReadableBufferInterface $raw
+     * @param string|ReadableBufferInterface $raw
      * @return static
      */
-    public static function decodeRawTransaction(Ethereum $eth, ReadableBufferInterface $raw): static;
+    public static function decodeRawTransaction(string|ReadableBufferInterface $raw): static;
 
     /**
-     * @return static
+     * @return string
+     */
+    public function encode(): string;
+
+    /**
+     * @return static The unsigned version of this transaction.
      */
     public function getUnsigned(): static;
 
     /**
-     * @return bool
+     * @return bool Whether the transaction is signed or not.
      */
     public function isSigned(): bool;
 
     /**
-     * @return Bytes32
+     * @return Bytes32 Keccak256 hash of the UNSIGNED transaction bytes.
      */
     public function signPreImage(): Bytes32;
 
     /**
-     * @return Bytes32
+     * @return Bytes32 Keccak256 hash of the transaction bytes.
      */
     public function hash(): Bytes32;
 }
-
