52171c109ca60bb1d154de9f37a760a6fe748b0f
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-11 17:03:03 +0400

    feat: implement ContractDtoInterface in ContractEvent with DTO transformation methods

--- src/Evm/ContractEvent.php
+++ src/Evm/ContractEvent.php
@@ -12,7 +12,7 @@ namespace FurqanSiddiqui\Ethereum\Evm;
  * Represents a contract event, encapsulating its name, anonymity status,
  * and associated input parameters.
  */
-final class ContractEvent
+final class ContractEvent implements ContractDtoInterface
 {
     use ContractEntitySignatureTrait;
 
@@ -40,5 +40,58 @@ final class ContractEvent
     public function appendInput(AbiParam $param): void
     {
         $this->inputs[] = $param;
+    }
+
+    /**
+     * @return array
+     */
+    public function toDto(): array
+    {
+        $abi = ["name" => $this->name, "type" => "event", "inputs" => []];
+        if (is_bool($this->isAnonymous)) {
+            $abi["anonymous"] = $this->isAnonymous;
+        }
+
+        foreach ($this->inputs as $input) {
+            $abi["inputs"][] = $input->toDto();
+        }
+
+        return $abi;
+    }
+
+    /**
+     * @param array $abi
+     * @return self
+     */
+    public static function fromDto(array $abi): self
+    {
+        if (!$abi) {
+            throw new \InvalidArgumentException("ContractEvent expects non-empty array");
+        } elseif (!isset($abi["type"]) || $abi["type"] !== "event") {
+            throw new \InvalidArgumentException("Invalid type for ContractEvent");
+        } elseif (!isset($abi["name"]) || !is_string($abi["name"]) || $abi["name"] === "") {
+            throw new \InvalidArgumentException("Invalid name for ContractEvent");
+        }
+
+        if (array_key_exists("anonymous", $abi) && !is_bool($abi["anonymous"])) {
+            throw new \InvalidArgumentException("Invalid value for anonymous for ContractEvent");
+        }
+
+        $event = new self($abi["name"], $abi["anonymous"] ?? null);
+        $inputs = $abi["inputs"] ?? [];
+        if (!is_array($inputs)) {
+            throw new \InvalidArgumentException("Invalid inputs for ContractEvent");
+        }
+
+        foreach ($inputs as $index => $input) {
+            try {
+                $event->appendInput(AbiParam::fromDto($input));
+            } catch (\Exception $e) {
+                throw new \InvalidArgumentException(sprintf("Invalid input[%d] for ContractEvent: %s", $index, $abi["name"]),
+                    previous: $e);
+            }
+        }
+
+        return $event;
     }
 }
