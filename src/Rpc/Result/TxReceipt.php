ceb391ceed4126b9ec97b2f0568976e8e4faf6da
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-11 21:42:19 +0400

    refactor: update TxReceipt to use strong types with Bytes32, EthereumAddress, and BufferImmutable; improve validation and exception handling

---
+++ src/Rpc/Result/TxReceipt.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * Part of the "furqansiddiqui/ethereum-php" package.
+ * @link https://github.com/furqansiddiqui/ethereum-php
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\Ethereum\Rpc\Result;
+
+use Charcoal\Buffers\BufferImmutable;
+use Charcoal\Buffers\Types\Bytes32;
+use FurqanSiddiqui\Ethereum\Keypair\EthereumAddress;
+
+/**
+ * Represents a transaction receipt within a blockchain context, providing details
+ * about a transaction's execution and its resulting effects.
+ */
+final readonly class TxReceipt
+{
+    public Bytes32 $transactionHash;
+    public int $transactionIndex;
+    public Bytes32 $blockHash;
+    public int $blockNumber;
+    public int $cumulativeGasUsed;
+    public int $gasUsed;
+    public ?EthereumAddress $contractAddress;
+    public bool $status;
+    public BufferImmutable $logsBloom;
+
+    /** @var TxReceiptLog[] */
+    public array $logs;
+
+    /**
+     * @param array $result
+     */
+    public function __construct(array $result)
+    {
+        if (!isset($result["transactionHash"], $result["transactionIndex"], $result["blockHash"],
+            $result["blockNumber"], $result["cumulativeGasUsed"], $result["gasUsed"],
+            $result["logs"],$result["logsBloom"])) {
+            throw new \InvalidArgumentException("Incomplete tx receipt data");
+        }
+
+        $this->transactionHash = new Bytes32($result["transactionHash"]);
+        $this->transactionIndex = gmp_intval(gmp_init($result["transactionIndex"], 16));
+        $this->blockHash = new Bytes32($result["blockHash"]);
+        $this->blockNumber = gmp_intval(gmp_init($result["blockNumber"], 16));
+        $this->cumulativeGasUsed = gmp_intval(gmp_init($result["cumulativeGasUsed"], 16));
+        $this->gasUsed = gmp_intval(gmp_init($result["gasUsed"], 16));
+        $this->contractAddress = ($result["contractAddress"] ?? null)
+            ? new EthereumAddress($result["contractAddress"])
+            : null;
+
+        $this->logsBloom = new BufferImmutable($result["logsBloom"]);
+        if (isset($result["status"])) {
+            $this->status = $result["status"] === "0x1";
+        } elseif (isset($result["root"])) {
+            $this->status = true;
+        } else {
+            throw new \InvalidArgumentException("Missing receipt status/root");
+        }
+
+        $receiptLogs = [];
+        foreach ($result["logs"] as $index => $log) {
+            $receiptLogs[] = new TxReceiptLog($index, $log);
+        }
+
+        $this->logs = $receiptLogs;
+    }
+}
