dd2436e3647030078cf22119021288eb6a89d447
Furqan Siddiqui <hello@furqansiddiqui.com>
2026-02-11 21:45:22 +0400

    refactor: improve TxReceiptLog validation, replace StringVector with native array for topics, and enhance exception handling

--- src/Rpc/Result/TxReceiptLog.php
+++ src/Rpc/Result/TxReceiptLog.php
@@ -1,19 +1,23 @@
 <?php
+/*
+ * Part of the "furqansiddiqui/ethereum-php" package.
+ * @link https://github.com/furqansiddiqui/ethereum-php
+ */
+
 declare(strict_types=1);
 
-namespace FurqanSiddiqui\Ethereum\RPC\Result;
-
-use Charcoal\OOP\Vectors\StringVector;
-use FurqanSiddiqui\Ethereum\Exception\RPC_ResponseException;
+namespace FurqanSiddiqui\Ethereum\Rpc\Result;
 
 /**
- * Class TxReceiptLog
- * @package FurqanSiddiqui\Ethereum\RPC\Result
+ * Represents a transaction receipt log on the blockchain. This class encapsulates
+ * details about a specific log entry within a transaction receipt, providing access
+ * to its address, topics, data, and other metadata such as block and transaction
+ * details.
  */
-readonly class TxReceiptLog
+final readonly class TxReceiptLog
 {
     public string $address;
-    public StringVector $topics;
+    public array $topics;
     public string $data;
     public int $blockNumber;
     public string $transactionHash;
@@ -25,18 +29,21 @@ readonly class TxReceiptLog
     /**
      * @param int $index
      * @param array $result
-     * @throws RPC_ResponseException
      */
     public function __construct(int $index, array $result)
     {
         if (!isset($result["address"], $result["topics"], $result["data"], $result["blockNumber"],
             $result["transactionHash"], $result["transactionIndex"], $result["blockHash"], $result["logIndex"],
             $result["removed"])) {
-            throw new RPC_ResponseException("Incomplete tx receipt log # $index data");
+            throw new \InvalidArgumentException("Incomplete TxReceiptLog data at index: " . $index);
+        } elseif (!is_bool($result["removed"])) {
+            throw new \InvalidArgumentException("Invalid removed flag at index: " . $index);
+        } elseif (!is_array($result["topics"])) {
+            throw new \InvalidArgumentException("Invalid topics at index: " . $index);
         }
 
         $this->address = $result["address"];
-        $this->topics = new StringVector(...$result["topics"]);
+        $this->topics = $result["topics"];
         $this->data = $result["data"];
         $this->blockNumber = gmp_intval(gmp_init($result["blockNumber"], 16));
         $this->transactionHash = $result["transactionHash"];
